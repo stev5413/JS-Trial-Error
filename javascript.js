61149b80d54ea8d7e405df1b40201f570623f472
stev5413 <78006810+stev5413@users.noreply.github.com>
2025-03-24 14:47:32 -0500

    Update 3-24-2025
    DOM manipulation lessons

---
+++ javascript.js
@@ -0,0 +1,29 @@
+//This function does not put a new into the DOM but into memory
+const p = document.createElement('p');
+const h3 = document.createElement('h3');
+const div = document.createElement('div');
+
+
+//console.dir(p);
+
+document.body.append(p);
+p.textContent = "Hey, I'm red";
+
+
+document.body.append(h3);
+h3.textContent = "I'm a blue h3";
+h3.style.color = "blue";
+
+document.body.append(div);
+div.style.cssText = "border: black; background: pink;";
+
+
+const h1 = document.createElement('h1');
+div.appendChild(h1);
+h1.textContent = "I'm in a div";
+
+
+const p2 = document.createElement('p');
+div.appendChild(p2);
+p2.textContent = "ME TOO!";
+
