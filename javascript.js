2fae0e5b9c84e132778798fef0a3a5a71b9dffbc
stev5413 <78006810+stev5413@users.noreply.github.com>
2025-03-25 19:55:12 -0500

    03-25-25
    DOM manipulation, adding HTML content

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 //This function does not put a new into the DOM but into memory
+
 const p = document.createElement('p');
 const h3 = document.createElement('h3');
 const div = document.createElement('div');
@@ -6,6 +7,7 @@ const div = document.createElement('div');
 
 //console.dir(p);
 
+//03-24-25
 document.body.append(p);
 p.textContent = "Hey, I'm red";
 
@@ -17,13 +19,23 @@ h3.style.color = "blue";
 document.body.append(div);
 div.style.cssText = "border: black; background: pink;";
 
-
 const h1 = document.createElement('h1');
 div.appendChild(h1);
 h1.textContent = "I'm in a div";
 
-
 const p2 = document.createElement('p');
 div.appendChild(p2);
 p2.textContent = "ME TOO!";
+
+
+//03-25-25
+const container = document.querySelector("#container");
+
+const content = createElement("div");
+content.classList.add("content");
+
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
 
