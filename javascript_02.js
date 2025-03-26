332ca324ad5d5c03799732771eb7af276a4da856
stev5413 <78006810+stev5413@users.noreply.github.com>
2025-03-26 13:16:55 -0500

    Attaching listeners to groups of nodes
    Iterate through the nodes, add event listener to each one

--- javascript_02.js
+++ javascript_02.js
@@ -6,3 +6,38 @@ content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
 
 container.appendChild(content);
+
+//3-26-25
+/*
+const btn = document.querySelector('#btn');
+btn.onclick = () => alert("Hello World");
+*/
+
+const btnTwo = document.querySelector('#btnTwo');
+btnTwo.addEventListener("click", () => {
+    alert("Hello World back to you");
+});
+
+//Using functions and event listener
+/*
+btn.addEventListener("click", function (e) {
+    console.log(e.target);
+});
+*/
+
+
+btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+});
+
+const buttons = document.querySelectorAll("button");
+
+//call the const, use forEach to iterate through the "button elements"
+buttons.forEach((button) => {
+    //adds a click listener to each button
+    button.addEventListener("click", () => {
+        alert(button.id);
+    });
+});
+
+
