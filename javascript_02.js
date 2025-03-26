4daf860083d837cd7d60eb4e2f645adbe4b94d02
stev5413 <78006810+stev5413@users.noreply.github.com>
2025-03-26 09:00:52 -0500

    03-25-25
    Added additional HTML index and javascript file

---
+++ javascript_02.js
@@ -0,0 +1,8 @@
+//03-25-25
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
